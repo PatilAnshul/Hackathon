8ab5bce7f25b3b71ea5229f4ab1ce24283107220
Anshul <111892376+PatilAnshul@users.noreply.github.com>
2023-09-18 03:59:17 +0530

    Add files via upload

---
+++ knowledgebase.py
@@ -0,0 +1,12 @@
+{
+    "questions": [
+      {
+        "questions": "hi",
+        "answer": "hello! how are you"
+      },
+      {
+        "questions": "yo",
+        "answer": "hello"
+      }
+    ]
+  }
